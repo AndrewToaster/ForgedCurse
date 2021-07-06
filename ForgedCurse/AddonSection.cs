3a59d93b5a59e0823f822a3585b3fd1eb36c8c1a
AndrewToaster <29014953+AndrewToaster@users.noreply.github.com>
2021-07-06 12:09:55 +0200

    Fixed AddonCategory enum class

---
+++ ForgedCurse/AddonSection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForgedCurse
+{
+    /// <summary>
+    /// A section class containing method to retrieve information for Addons
+    /// </summary>
+    public sealed class AddonSection
+    {
+        /// <summary>
+        /// The base <see cref="ForgeClient"/>
+        /// </summary>
+        public ForgeClient Client { get; }
+
+        internal AddonSection(ForgeClient client)
+        {
+            Client = client;
+        }
+    }
+}
